8d7b6d2885a5bf5c779ced4cb8bc721d80dce2d8
jacobpachev <jacobpachev@gmail.com>
2020-08-10 18:28:32 -0600

    added piglatin to english feature

--- piglatin.js
+++ piglatin.js
@@ -3,9 +3,10 @@ function uc_first(s){
 	return s[0].toUpperCase() + l.toLowerCase();  
 }
 
+var vowel = "aeiou";
+var punc = ",.!?:;-";
+
 function pig_latin(word) {
-    var vowel = "aeiou";
-    var punc = ",.!?:;-";
     var p = "";
 	if(!word){
 		return '';    
@@ -16,6 +17,7 @@ function pig_latin(word) {
     if(vowel.includes(word[0])) {
         p = word + "hay";
     }
+    else if(word[0]=="h") p = word.slice(1) + "-" + word[0] + "ay"
     else {
         p = word.slice(1) + word[0] + "ay";   
     }
@@ -23,9 +25,39 @@ function pig_latin(word) {
 	if(word[0] == c){
 		return uc_first(p);
 	}
-	console.log(uc_first("HEllo")) 
     return p;
 
+}
+function validPigLatin(word){
+    if(word.length < 3) return false;
+    if(word.slice(word.length-2) != "ay") return false;
+    if(vowel.includes(word[word.length-3])) return false;
+    return true;
+}
+
+function inverse_piglatin(word){
+    var n = "";
+    if(!word) return word;
+    if(punc.includes(word[word.length-1])){
+        return inverse_piglatin(word.slice(0,word.length-1)) + word[word.length-1];
+	}
+    if(!validPigLatin(word)) return word;
+	if(word[word.length-3] == "h"){
+        if(word[word.length-4] == "-"){
+            n = word[word.length-3] + word.slice(0,word.length-4);
+        }
+        else {
+           n = word.slice(0,word.length-3);
+        }
+    }
+    else{
+        n = word[word.length-3] +  word.slice(0,word.length-3);
+    }
+    var c = word[0].toUpperCase()
+	if(word[0] == c){
+		return uc_first(n);
+	}
+    return n;
 }
 
 function pig_latin_phrase(phrase){
@@ -36,6 +68,17 @@ function pig_latin_phrase(phrase){
 	}
 	return translation.join(" ");
 }
+
+function inverse_pig_latin_phrase(phrase){
+	phrase = phrase.split(" ");
+	var translation = []
+	for (var i = 0; i <= phrase.length; i++){
+		translation.push(inverse_piglatin(phrase[i]));
+	}
+	return translation.join(" ");
+}
+
+console.log(inverse_pig_latin_phrase("Ello-hay orldway, ello-hay orldway"));
 
 function handle_input()
 {
@@ -48,40 +91,24 @@ new Vue({
     el: '#app',
     vuetify: new Vuetify(),
     data: {
-       input: ""
+        english: "",
+        pig: "",
     },
 
-    computed: {
+    methods: {
+        updateEnglish(val) {
+            if (!val) this.english = null;
+            this.english = inverse_pig_latin_phrase(val);
+        },
+        updatePig(val) {
+            if (!val) this.english = null;
+            this.pig = pig_latin_phrase(val);
+        }
+    },
+/*    computed: {
         translation() {
             if (!this.input) return null;
             return pig_latin_phrase(this.input);
         }
-    }
-})
-
-/*
-def uc_first(s):
-    return s[0].upper() + s[1:].lower()
-
-def pig_latin(word):
-    vowel = "aeiou"
-    punc = ",.!?;:-"
-    if len(word) == 0:
-        return word
-    if word[-1] in punc:
-        return pig_latin(word[:-1]) + word[-1]
-    if word[0] in vowel:
-        p = word + "hay"
-    else:
-        p = word[1:] + word[0] + "ay"
-    if word[0].isupper():
-        p = uc_first(p)
-    return p
-    
-def pig_latin_phrase(phrase):
-    phrase = phrase.split(" ")
-    translation = []  
-    for w in phrase:
-        translation.append(pig_latin(w))
-    return " ".join(translation)*/
- 
+    }*/
+});
