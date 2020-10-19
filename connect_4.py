6205d53c33b4cd8743c2c4a47d5fed301bde8fe1
Joseph Pachev <josephpachev@gmail.com>
2020-10-19 15:58:15 -0600

    win method works for rows and columns

--- connect_4.py
+++ connect_4.py
@@ -2,27 +2,31 @@ import numpy as np
 
 WHITE = 1
 BLACK = 2
-width = 4
-height = 5
 
 def play_game():
-    g = ConnectFour()
+    g = ConnectFour(width=4, height=5s)
     while True:
         if g.turn:
             print("White's move:")
             g.move(int(input()))
             g.print_board()
+            if g.game_over: return
         else:
             print("Black's move:")
             g.move(int(input()))
             g.print_board()
+            if g.game_over: return
 
 class ConnectFour():
     
-    def __init__(self):
+    def __init__(self, width=4, height=5):
         self.heights = np.zeros(4, dtype=int)
-        self.board = np.zeros((5, 4), dtype=int)
+        self.board = np.zeros((height, width), dtype=int)
         self.turn = True
+        self.game_over = False
+        self.width = width
+        self.height = height
+
         
     def move(self,  pos):
         self.board[self.heights[pos], pos] = WHITE if self.turn else BLACK
@@ -37,10 +41,20 @@ class ConnectFour():
         self.turn = not self.turn
     
     def win(self):
-        for i in range(height):
-            if np.all(self.board[i] == 1): print("White wins!") 
-            elif np.all(self.board[i] == 2): print("Black wins!")
-            else: return
+        for i in range(self.height):
+            self.check_slice(self.board[i])
+        for j in range(self.width):
+            for k in range(self.height - 3):
+                self.check_slice(self.board[k:k+4, j])
+
+    def check_slice(self, ar):
+        if np.all(ar == 1): 
+            print("White wins!")
+            self.game_over = True
+        elif np.all(ar == 2): 
+            print("Black wins!")
+            self.game_over = True
+        else: return
 
 if __name__ == "__main__":
     #game = ConnectFour()
