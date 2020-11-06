338bfc340c161d3e5025dfc77037c3f080f12913
Joseph Pachev <josephpachev@gmail.com>
2020-11-06 16:50:00 -0700

    testcase maker complete

--- testcase.py
+++ testcase.py
@@ -1,20 +1,47 @@
 import pillars as  p
 
-def make_testcase(fname):
-    g = p.PillarsOfPlato(width=4, length=4, height=5)
+def random_game(mute=False):
+    g = p.PillarsOfPlato(width=4, length=4, height=5, mute=mute)
+    move = []
     while True:
         g.rand_move()
-        print(g.last_move)
-        with open(fname, "w") as f:
-            f.write(str(g.last_move))
-        
+        #print(g.last_move)
+        move.append(g.last_move)
         if g.game_over:
             if g.turn:
                 winner = 2
             else:
                 winner = 1
-            print(winner)    
-            return
-        
+            #print(winner)
+            break
+    return move, winner
+
+def make_testcase(fname):
+    move, winner = random_game()
+    with open(fname, "w") as f:
+            f.write(str(winner) + "\n")
+            for t in move:
+                f.write(" ".join(map(str, t)) + "\n")
+
+def make_testcases(n):
+    for i in range(n):
+        make_testcase(f"josephplatotests/test{i}.txt")
+
+def get_stats(n):
+    import numpy as np
+    lengths = np.zeros(n)
+    winners = np.zeros(n)
+    i = 0
+    print_stats = lambda: print(f"Average Game Length: {np.mean(lengths[:i+1])}, White win: {100*np.mean(winners[:i+1]==1)}")
+
+    for i in range(n):
+        move, winner = random_game(mute=True)
+        lengths[i] = len(move)
+        winners[i] = winner
+        if i%10 == 0: print(f"Processed {i+1}")
+        if i %100 == 0: print_stats()
+    print_stats()
+
 if __name__ == "__main__":
-    make_testcase("test")
+    make_testcases(50)
+    #get_stats(1000)
